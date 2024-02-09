55ff66b31ba8106ad8d2fa3c07e6e2246290af2d
francisco1337 <francisco.sdv.mtz@hotmail.com>
2024-02-09 15:21:05 -0600

    SECCION 5 FINALIZADA <3

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
         remotePatterns:[{
             protocol: 'https',
             hostname: 'images.unsplash.com'
-        }]
+        },
+        {
+            protocol: 'https',
+            hostname: 'raw.githubusercontent.com'
+        }
+    ]
     }
 };
 
